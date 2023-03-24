343197bb1ba886cfacd294a67b5968dbd99ba298
labakie <95135910+labakie@users.noreply.github.com>
2023-03-24 21:59:10 +0700

    Create handlers for add and get all books

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,19 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+} = require('./handler');
+
 const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
 ];
 
 module.exports = routes;
